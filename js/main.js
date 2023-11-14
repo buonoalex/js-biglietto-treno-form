80d7bd955f1803934db06bd3036d86dceae6b0b3
buonoalex <loopatthecity@gmail.com>
2023-11-14 17:20:16 +0100

    add and fixed function

--- js/main.js
+++ js/main.js
@@ -4,19 +4,26 @@ function LoginScreen(){
 
     document.getElementById("screenPrincipal").classList.add("d-none");
 
+    document.getElementById("LoginScreen").classList.remove("d-none");
 
+    document.getElementById("inputUtente").classList.add("d-none");
+
+    document.getElementById("bigliettiUtenti").classList.add("d-none");
+
+    document.getElementById("selezionaetà").classList.add("d-none");
 }
 
 function ComproScreen(){
 
     document.getElementById("screenPrincipal").classList.add("d-none");
 
+    document.getElementById("LoginScreen").classList.add("d-none");
+
     document.getElementById("inputUtente").classList.remove("d-none");
     
 }
 
-function BigliettoCompletato(event){
-    event.preventDefault();
+function BigliettoCompletato(){
 
     let kmpercorsi = parseInt(document.getElementById("inputKm").value);
     let email = document.getElementById("inputEmail").value;
@@ -25,32 +32,27 @@ function BigliettoCompletato(event){
     console.log("Input completati");
     console.log(choisePerson);
 
-   
-
     //Switch età
-    document.getElementById("selezionaetà").classList.remove("d-none");
-
-    let controll1 = false;
 
     switch(choisePerson){
         
         case "1":
             document.getElementById("utente2").classList.add("d-none");
             document.getElementById("utente3").classList.add("d-none");
-            controll1 = true;
+            document.getElementById("selezionaetà").classList.remove("d-none");
             console.log("eseguito1");
             
         break;
         
         case "2":
             document.getElementById("utente3").classList.add("d-none");
-            controll1 = true;
+            document.getElementById("selezionaetà").classList.remove("d-none");
             console.log("eseguito2");
             
         break;
 
         case "3":
-            controll1 = true;
+            document.getElementById("selezionaetà").classList.remove("d-none");
             console.log("eseguito3");
         break;
 
@@ -60,15 +62,51 @@ function BigliettoCompletato(event){
 
     }
 
-    //Switch Prize
-    if(controll1 == true){
-        document.getElementById("bigliettiUtenti").classList.remove("d-none");
+    return choisePerson;
+
+}
+
+function VistaBiglietti(){
+
+    let numberBiglietti = BigliettoCompletato()
+
+    let etapersona1 = document.getElementById("etaUtente1").value;
+    let etapersona2 = document.getElementById("etaUtente2").value;
+    let etapersona3 = document.getElementById("etaUtente3").value;
+
+    console.log("Eta confermate");
+
+    switch(numberBiglietti){
+        case "1":
+            document.getElementById("utentePrezzo2").classList.add("d-none");
+            document.getElementById("utentePrezzo3").classList.add("d-none");
+            document.getElementById("bigliettiUtenti").classList.remove("d-none");
+            console.log("Eseguito prize 1");
+        break;
+
+        case "2":
+            document.getElementById("utentePrezzo3").classList.add("d-none");
+            document.getElementById("bigliettiUtenti").classList.remove("d-none");
+            console.log("Eseguito prize 2");
+        break;
+
+        case "3":
+            document.getElementById("bigliettiUtenti").classList.remove("d-none");
+            console.log("Eseguito prize 3");
+        break;
+
     }
-    else{
-        alert("dio");
+}
+
+function CalcoloPrize(etaPerson , kmfatti){
+    const km = 0.21;
+    let result;
+    if(etaPerson <18){
+        result = (kmfatti * 20) / 100;
+    }else if(etaPerson>65){
+        result = (kmfatti * 40) / 100;
+    }else{
+        result = kmfatti * km;
     }
-   
-
-
-
+    return result;
 }
