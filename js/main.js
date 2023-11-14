eda951a812a7eddf993abcce45715294099a1ba3
buonoalex <loopatthecity@gmail.com>
2023-11-14 16:16:19 +0100

    try add some function

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ function LoginScreen(){
 
     document.getElementById("screenPrincipal").classList.add("d-none");
 
-    document.getElementById("inputUtente").classList.add("d-none");
 
 }
 
@@ -12,7 +11,7 @@ function ComproScreen(){
 
     document.getElementById("screenPrincipal").classList.add("d-none");
 
-    document.getElementById("LoginScreen").classList.add("d-none");
+    document.getElementById("inputUtente").classList.remove("d-none");
     
 }
 
@@ -24,6 +23,52 @@ function BigliettoCompletato(event){
     let choisePerson = document.getElementById("inputBiglietti").value;
 
     console.log("Input completati");
+    console.log(choisePerson);
 
-    
+   
+
+    //Switch età
+    document.getElementById("selezionaetà").classList.remove("d-none");
+
+    let controll1 = false;
+
+    switch(choisePerson){
+        
+        case "1":
+            document.getElementById("utente2").classList.add("d-none");
+            document.getElementById("utente3").classList.add("d-none");
+            controll1 = true;
+            console.log("eseguito1");
+            
+        break;
+        
+        case "2":
+            document.getElementById("utente3").classList.add("d-none");
+            controll1 = true;
+            console.log("eseguito2");
+            
+        break;
+
+        case "3":
+            controll1 = true;
+            console.log("eseguito3");
+        break;
+
+        default:
+            alert("Non hai inserito i numeri");
+        break;
+
+    }
+
+    //Switch Prize
+    if(controll1 == true){
+        document.getElementById("bigliettiUtenti").classList.remove("d-none");
+    }
+    else{
+        alert("dio");
+    }
+   
+
+
+
 }
